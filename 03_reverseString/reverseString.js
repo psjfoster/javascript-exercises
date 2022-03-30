dd2f262780722ce5f46a386555b4ec01c6cc607b
psjfoster <85462085+psjfoster@users.noreply.github.com>
2022-03-31 00:37:19 +0100

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let array = string.split("");
+  let stringReversed = "";
+  for (let i = array.length - 1; i >= 0; i--) {
+    stringReversed = stringReversed.concat(array[i]);
+  }
+  return stringReversed;
 };
 
 // Do not edit below this line
