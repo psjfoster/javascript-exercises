8acd3971638738c8ef0cc59d378fedf5b600c0bf
psjfoster <85462085+psjfoster@users.noreply.github.com>
2022-03-31 00:11:46 +0100

    Complete exercises 1&2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+  let newString = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < num; i++) {
+    newString = newString + string;
+  }
+  return newString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
