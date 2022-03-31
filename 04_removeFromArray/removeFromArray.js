f1a4d754f94c144a4d714a335ef0c7b50a4edab8
psjfoster <85462085+psjfoster@users.noreply.github.com>
2022-03-31 01:20:10 +0100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array) {
+  let newArray = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    let deleteItem = false;
+    for (let a = 1; a < arguments.length; a++) {
+      if (array[i] === arguments[a]) {
+        deleteItem = true;
+        break;
+      }
+    }
+    if (deleteItem == false) {
+      newArray = newArray.concat(array[i]);
+    }
+  }
+  return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
